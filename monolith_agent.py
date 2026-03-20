fd0e8b35dd9a1cc8c0cad6f93b2db08c47959229
Jhanvi Mistry <41504717+JhanviMistry@users.noreply.github.com>
2026-03-20 20:22:25 +0000

    Create monolith_agent.py

---
+++ monolith_agent.py
@@ -0,0 +1,13 @@
+import asyncio
+from google.adk.agents import Agents
+from google.adk.runner import InMemoryRunner
+from dotenv import load_dotenv
+
+load_dotenv()
+
+flight_agent = Agent()
+
+async def main():
+
+
+if 
