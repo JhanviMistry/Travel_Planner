10e7f1efa073b30814e10d028ef2be4194cf89bb
Jhanvi Mistry <41504717+JhanviMistry@users.noreply.github.com>
2026-03-23 13:02:33 +0000

    added agent details

--- monolith_agent.py
+++ monolith_agent.py
@@ -1,13 +1,20 @@
 import asyncio
-from google.adk.agents import Agents
-from google.adk.runner import InMemoryRunner
+from google.adk.agents import Agent
+from google.adk.runners import InMemoryRunner
 from dotenv import load_dotenv
 
 load_dotenv()
 
-flight_agent = Agent()
+flight_agent = Agent(
+    model = "gemini-2.5-flash",
+    name = "Flight_Agent",
+    description = "Tell me the best route between flights",
+)
 
 async def main():
+    runner = InMemoryRunner(agent = flight_agent)
+    events = await runner.run_debug("How many layovers are there between London and India?")
 
 
-if 
+if __name__ == "__main__":
+    asyncio.run(main())
